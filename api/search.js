7788fa0fc7d0724097726e57f1dd77e93726c619
Creatorpoint <pk4030794@gmail.com>
2026-03-04 18:32:31 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,17 +1,21 @@
 export default async function handler(req, res) {
+
   const { userid } = req.query;
 
-  if (!userid) {
+  if(!userid){
     return res.status(400).json({ error: "UserID required" });
   }
 
-  try {
+  try{
     const response = await fetch(
-      "https://tg-2-num-api-org.vercel.app/api/search?userid=" + userid
+      `https://tg-2-num-api-org.vercel.app/api/search?userid=${userid}`
     );
+
     const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: "API Failed" });
+
+    return res.status(200).json(data);
+
+  }catch(error){
+    return res.status(500).json({ error: "API Failed" });
   }
 }
